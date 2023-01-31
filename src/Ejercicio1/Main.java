8b5d69e89e2bc7ea5e2e3302e6abe011b6579ba4
leandrosarvi <leandromartinsarvi@hotmail.com>
2023-01-31 15:45:13 -0300

    fist commit

---
+++ src/Ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+Tipos();
+    }
+    public static void Tipos(){
+        int numero=5;
+        long log=55555;
+        String nombre = "leandro";
+        boolean ver = true;
+        double dou = 5.5;
+        System.out.println("Int: "+numero);
+        System.out.println("Long"+log);
+        System.out.println("String: "+nombre);
+        System.out.println("Boolean: "+ver);
+        System.out.println("Double: "+dou);
+
+    }
+}
